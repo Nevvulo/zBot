9353e8903614d7df216e2feb2460bdb582f9ddd5
zBlakee <bsward8@gmail.com>
2017-05-02 21:36:12 +1000

    Added more commands to mod help

--- commands/moderator/help.js
+++ commands/moderator/help.js
@@ -1,18 +1,19 @@
 exports.run = (client, message, args) => {
     var helpMessage = "**Fleece Police Commands only:**```\n" +
-        "* mod [on|off]    		Queries moderation status.\n";
+        "* mod		    		Toggles moderation status.\n";
 
     helpMessage = helpMessage +
-        "* filter [on|off]   	  Queries the chat filter.\n";
+        "* filter			   	Toggles the chat filter.\n";
 
     helpMessage = helpMessage +
         "rm [number]       		Deletes a number of messages.\n" +
         "uinfo [user]      		Gets information about a user.\n" +
-        "                  		You must mention the user in\n" +
-        "                  		order to see their information.\n" +
         "warn [user] [reason]      Give a warning to a user.\n" +
         "ban [user] [reason]       Ban someone from the server.\n" +
+		"mute [user] [reason]       Mute somebody on the server.\n" +
+		"softban [user] [reason]       Softban someone on the server.\n" +
         "say [message]	   	  Say something under the bot's name.\n" +
+		"permit [user]	   	  Permit a user to post any links for 3 minutes.\n" +
         "setgame [game]			Set what the bot is playing to whatever you want.\n" +
         "* panic     		      Toggles panic mode.\n" +
         "deletemessages    		Automatically removes bot messages after 10 seconds.\n" +
@@ -21,6 +22,7 @@ exports.run = (client, message, args) => {
         "reboot            		Asks XailBot to reconnect.\n" +
         "\n" +
         "* = denotes an admin only command\n" +
+		"The user argument currently only works when said user is mentioned.\n" +
         "These commands need to be prefixed with mod:\n" +
         "```";
     message.delete();
