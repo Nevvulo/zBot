db7a12048adf48955b7f035cae0117c0e8707aa5
zBlakee <bsward8@gmail.com>
2017-05-04 22:28:23 +1000

    Made mod:rm more efficient and added 'everything' argument
    'everything' argument clones the channel and deletes it to remove ALL messages.

--- commands/moderator/rm.js
+++ commands/moderator/rm.js
@@ -1,8 +1,24 @@
+var deleteEverythingConfirm = false;
+
 exports.run = (client, message, args) => {
  doNotDelete = false;
 
+if (args.toString() == "everything") {
+if (deleteEverythingConfirm == false) {
+message.channel.send(":no_entry: **HOLD UP:** Doing this command will result in this channel being **deleted**, then **re-created**. This will remove all messages. If you are aware of what this command will do, you can type `mod:rm everything` again.");
+deleteEverythingConfirm = true;
+return;
+}
+	message.channel.clone();
+	message.channel.delete();
+	
+	var delchannel = message.channel;
+	
+	client.channels.get("196793479899250688").sendMessage(":white_check_mark: **OK:** I successfully deleted the channel you were just in and re-created it. All messages in the channel have been removed.");
+	return;
+}
+
 var num = parseInt(args);
-console.log(args);
 
 if (num != args) {
 	message.channel.send(":no_entry_sign: **ERROR:** That's not a number...");
@@ -11,7 +27,6 @@ if (num != args) {
 	message.channel.bulkDelete(num).then(function() {
 
 		message.channel.send(":white_check_mark: **OK:** I successfully deleted " + parseInt(num - 1) + " messages.");
-		numDel = parseInt(num - 1);
 	}).catch(function(reason) {
 		console.log(reason);
 		switch (Math.floor(Math.random() * 1000) % 4) {
