0f4444c0c1398c71463f7855ab374609412529ba
Blake <bsward8@gmail.com>
2017-05-28 00:47:25 +1000

    Ping/pong now displays more technical information

--- commands/ping.js
+++ commands/ping.js
@@ -4,31 +4,8 @@ function warningIcon(guild) {
     return ":warning:";
 }
 
-	if (message.author.id == "246574843460321291") {
-	message.channel.send('Ponging...').then(sent => {
-		sent.edit((warningIcon(message.guild)) + ` **PONG!** Xail Bot took **__${sent.createdTimestamp - message.createdTimestamp}ms__** to respond.`);
+	message.channel.send('Pinging...').then(sent => {
+		sent.edit((warningIcon(message.guild)) + ` **PONG!** Xail Bot *(message)* took **__${sent.createdTimestamp - message.createdTimestamp}ms__** to respond. Heartbeat acknowledgement *(client)* took **__${client.ping}ms__** to respond.`);
 	});
 	return;
-	}
-	
-
-doNotDelete = true;
-	switch (Math.floor(Math.random() * 1000) % 5) {
-
-		case 0:
-			message.channel.send(warningIcon(message.guild) + ' **PONG!** I want to play pong too... :\'(');
-			break;
-		case 1:
-			message.channel.send(warningIcon(message.guild) + ' **PONG!** I love playing pong!');
-			break;
-		case 2:
-			message.channel.send(warningIcon(message.guild) + ' **PONG!** Thanks for playing pong with me!');
-			break;
-		case 3:
-			message.channel.send(warningIcon(message.guild) + ' **PONG!** Reflect upon this!');
-			break;
-		case 4:
-			message.channel.send(warningIcon(message.guild) + ' **PONG!**');
-			break;
-	}
 }
--- commands/pong.js
+++ commands/pong.js
@@ -4,31 +4,8 @@ function warningIcon(guild) {
     return ":warning:";
 }
 
-	if (message.author.id == "246574843460321291") {
 	message.channel.send('Pinging...').then(sent => {
-		sent.edit((warningIcon(message.guild)) + ` **PING!** Xail Bot took **__${sent.createdTimestamp - message.createdTimestamp}ms__** to respond.`);
+		sent.edit((warningIcon(message.guild)) + ` **PING!** Xail Bot *(message)* took **__${sent.createdTimestamp - message.createdTimestamp}ms__** to respond. Heartbeat acknowledgement *(client)* took **__${client.ping}ms__** to respond.`);
 	});
 	return;
-	}
-	
-
-doNotDelete = true;
-switch (Math.floor(Math.random() * 1000) % 5) {
-	
-	case 0:
-		message.channel.send(warningIcon(message.guild) + ' **PING!** Pings are also cool!');
-		break;
-	case 1:
-		message.channel.send(warningIcon(message.guild) + ' **PING!** Do you like playing pong?');
-		break;
-	case 2:
-		message.channel.send(warningIcon(message.guild) + ' **PING!** Here\'s the test message you wanted!');
-		break;
-	case 3:
-		message.channel.send(warningIcon(message.guild) + ' **PING!** Don\'t breathe this!');
-		break;
-	case 4:
-		message.channel.send(warningIcon(message.guild) + ' **PING!**');
-		break;
-}
 }
