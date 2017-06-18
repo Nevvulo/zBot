2631d04a4c1d1fa5360ef9a12781719a11702e01
Blake <bsward8@gmail.com>
2017-06-18 13:06:49 +1000

    '@mention' is no longer required for "user" argument.

--- commands/moderator/permit.js
+++ commands/moderator/permit.js
@@ -1,32 +1,61 @@
 var permitMember = {};
 
 function permitRemove() {
-    permitMember.removeRole(permitMember.guild.roles.get("304207299646586882"));
-    console.log("Permitted role revoked.");
+	permitMember.removeRole(permitMember.guild.roles.get("304207299646586882"));
+	console.log("Permitted role revoked.");
 }
 
 exports.run = (client, message, args) => {
-doNotDelete = true;
-args = args.toString();
-args = args.replace("<", "").replace(">", "").replace("@", "").replace("!", "").replace(/[^0-9.]/g, "");
+	var num = 180000
+		args = args.toString();
 
-var num = 180000
-message.guild.fetchMember(args.split(" ").toString()).then(function(member) {
-	permitMember = member;
-	message.channel.send(":white_check_mark: **" + permitMember.displayName + "** is now permitted to post *any* links for 3 minutes.");
-	permitMember.addRole(permitMember.guild.roles.get("304207299646586882"));
-	console.log("Permitted role assigned.");
-	client.setTimeout(permitRemove, num);
-	message.delete();
-}).catch(function(reason) {
-	console.log(reason);
-	if (permitMember == null) {
-			message.reply(':no_entry_sign: **ERROR:** You need to enter a user to permit. See mod:help for more information.');
-			message.delete();
-			return;
+	//USERNAME/MENTION SYSTEM
+	if (args == "") {
+		args = message.author.id;
+	} else {
+		function getUserID(user) {
+			var u = user;
+			if (user.user != null) {
+				u = user.user;
 			}
-	
-	switch (Math.floor(Math.random() * 1000) % 4) {
+			return u.id;
+		}
+
+		args = args.replace(",", " ").replace(",", " ").replace(",", " ").toString();
+
+		if (!args.includes("<")) {
+			var foundUsers = client.users.findAll("username", args);
+			if (foundUsers.length == 0) {
+				message.channel.send(':no_entry_sign: **ERROR:** Couldn\'t find anyone with that username. You might want to try again.');
+				return;
+			} else {
+				for (let user of foundUsers) {
+					args = getUserID(user);
+				}
+			}
+		} else {
+			args = args.replace("<", "").replace(">", "").replace("@", "").replace("!", "").replace(/[^0-9.]/g, "");
+			console.log("Username not provided for arguments.");
+		}
+	}
+
+	message.guild.fetchMember(args).then(function (member) {
+
+		permitMember = member;
+		message.channel.send(":white_check_mark: **OK:** *" + permitMember.displayName + "* is now exempt from the link filtere for 3 minutes.");
+		permitMember.addRole(permitMember.guild.roles.get("304207299646586882"));
+		console.log("Permitted role assigned.");
+		client.setTimeout(permitRemove, num);
+		message.delete ();
+	}).catch (function (reason) {
+		console.log(reason);
+		if (permitMember == null) {
+			message.reply(':no_entry_sign: **ERROR:** You need to enter a user to permit. See mod:help for more information.');
+			message.delete ();
+			return;
+		}
+
+		switch (Math.floor(Math.random() * 1000) % 4) {
 		case 0:
 			message.channel.send(':no_entry_sign: **ERROR:** That didn\'t work. You might want to try again.');
 			break;
@@ -39,6 +68,6 @@ message.guild.fetchMember(args.split(" ").toString()).then(function(member) {
 		case 3:
 			message.channel.send(':no_entry_sign: **ERROR:** We are experiencing technical difficulties. You might want to try again.');
 			break;
-	}
-});
+		}
+	});
 }
