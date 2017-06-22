388beefe3081e5f1a33d007cb8039fa85f1d10d2
Blake <bsward8@gmail.com>
2017-06-22 20:44:55 +1000

    Updated 'info' command.
    More changes to this command are planned.

--- commands/info.js
+++ commands/info.js
@@ -1,24 +1,42 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 var colors = require('colors');
+var footer = ""
 
 exports.run = (client, message, args) => {
-embed = new Discord.RichEmbed("test");
-embed.setAuthor("ʙᴏᴛ ꜱᴛᴀᴛɪꜱᴛɪᴄꜱ");
-embed.setColor("#f4bf42"); {
+	embed = new Discord.RichEmbed("test");
+	embed.setAuthor("ʙᴏᴛ ꜱᴛᴀᴛɪꜱᴛɪᴄꜱ");
+	embed.setColor("#f4bf42"); {
 
-var uptime = Math.round(client.uptime / 60000);
-var minOrHour = "minutes";
+		var uptime = Math.round(client.uptime / 60000);
+		var minOrHour = "minute(s)";
 
-if (client.uptime > 3600000) {
-uptime = Math.round(client.uptime / 600000);
-minOrHour = "hours";
-}
+		if (client.uptime > 3600000) {
+			uptime = Math.round(client.uptime / 3600000);
+			minOrHour = "hour(s)";
+		}
 
-let data = `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} / ${Math.round(process.memoryUsage().heapTotal / 1024)}MB`;
-var emsg = "**Uptime**: " + uptime + " " + minOrHour + "\n**Logged in as user:** " + client.user.username + "\n**Memory Usage:** " + data;
-	embed.addField("Bot Debug Information", emsg);
-}
+		switch (Math.floor(Math.random() * 1000) % 4) {
+		case 0:
+			footer = "The picture used in 'bot:profile' is the same picture used in my profile picture!";
+			break;
+		case 1:
+			footer = "Xail Bot is developed in JavaScript using the Discord.js library.";
+			break;
+		case 2:
+			footer = "Suggest new ideas for Xail Bot on GitHub, or DM @zBlake#6715!";
+			break;
+		case 3:
+			footer = "Xail Bot has 'Who wants to be a millionaire' built in to it!";
+			break;
+		}
 
-message.channel.sendEmbed(embed);
+
+		let data = `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`;
+		var emsg = "**Uptime**: " + uptime + " " + minOrHour + "\n**Logged in as user:** " + client.user.username + "\n**Memory Usage:** " + data;
+		embed.addField("Bot Debug Information", emsg);
+		embed.setFooter(footer);
+	}
+
+	message.channel.sendEmbed(embed);
 }
