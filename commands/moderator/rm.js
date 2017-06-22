c17a0e3ab1f878aaa5a6535c44c4376f6a996a5b
Blake <bsward8@gmail.com>
2017-06-22 20:44:04 +1000

    mod:rm messages automatically get deleted.
    "x messages deleted" messages will now automatically get removed after 8 seconds or so.

--- commands/moderator/rm.js
+++ commands/moderator/rm.js
@@ -3,10 +3,26 @@ var deleteEverythingConfirm = false;
 exports.run = (client, message, args) => {
  doNotDelete = false;
 
+if (args.toString() == "bot") {
+const filter = msg => msg.author.bot == true;
+message.delete();
+		message.channel.fetchMessages({
+			limit: 99
+		}).then(messages => {
+			const filteredMessages = messages.filter(filter);
+			message.channel.bulkDelete(filteredMessages);
+			message.channel.send(":white_check_mark: **OK:** I've deleted all messages created by Xail Bot.").then(message => {
+        message.delete({ timeout: 8000 });
+      });
+		})
+	return;
+} 
+
 if (args.toString() == "everything") {
 if (deleteEverythingConfirm == false) {
 message.channel.send(":no_entry: **HOLD UP:** Doing this command will result in this channel being **deleted**, then **re-created**. This will remove all messages. If you are aware of what this command will do, you can type `mod:rm everything` again.");
 deleteEverythingConfirm = true;
+message.delete();
 return;
 }
 	message.channel.clone();
@@ -14,7 +30,9 @@ return;
 	
 	var delchannel = message.channel;
 	
-	client.channels.get("196793479899250688").sendMessage(":white_check_mark: **OK:** I successfully deleted the channel you were just in and re-created it. All messages in the channel have been removed.");
+	client.channels.get("196793479899250688").sendMessage(":white_check_mark: **OK:** I successfully deleted the channel you were just in and re-created it. All messages in the channel have been removed.").then(message => {
+        message.delete({ timeout: 8000 });
+      });
 	return;
 }
 
@@ -25,8 +43,9 @@ if (num != args) {
 } else {
 	num = num + 1; //Also remove the mod:rm command
 	message.channel.bulkDelete(num).then(function() {
-
-		message.channel.send(":white_check_mark: **OK:** I successfully deleted " + parseInt(num - 1) + " messages.");
+		message.channel.send(":white_check_mark: **OK:** I successfully deleted " + eval(num - 1) + " messages.").then(message => {
+        message.delete({ timeout: 8000 });
+      });
 	}).catch(function(reason) {
 		console.log(reason);
 		switch (Math.floor(Math.random() * 1000) % 4) {
