443f377d8fe71a736df31e227e8f037cbdafd1a8
Blake <bsward8@gmail.com>
2017-06-22 20:42:09 +1000

    Messages now automatically get deleted.
    Toggling AFK mode will now automatically delete the "AFK status" message after 8 seconds.

--- commands/afk.js
+++ commands/afk.js
@@ -1,18 +1,24 @@
+const afk = require('./../bot.js');
+
 exports.run = (client, message, args) => {
 doNotDelete = true;
 
-if (userAFK.includes(message.author.id)) {
-	message.reply(":white_check_mark: **OK:** You are no longer AFK.");
-	var index = userAFK.indexOf(message.author.id);
+if (afk.userAFK.includes(message.author.id)) {
+	message.reply(":white_check_mark: **OK:** You are no longer AFK.").then(message => {
+        message.delete({ timeout: 8000 });
+      });;
+	var index = afk.userAFK.indexOf(message.author.id);
 	if (index !== -1) {
-		userAFK.splice(index, 1);
+		afk.userAFK.splice(index, 1);
 	}
 	message.delete();
 	return;
 }
 
-message.reply(":white_check_mark: **OK:** I've set your status to AFK. If people mention you in their message, I'll notify them that you are AFK.");
-userAFK.push(message.author.id);
+message.reply(":white_check_mark: **OK:** I've set your status to AFK. If people mention you in their message, I'll notify them that you are AFK.").then(message => {
+        message.delete({ timeout: 8000 });
+      });
+afk.userAFK.push(message.author.id);
 message.delete();
 return;
 }
