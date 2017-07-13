1353ca47aa05cee2db73836a5390d1f7539cfa65
Blake <bsward8@gmail.com>
2017-07-13 14:00:51 +1000

    Warranty command no longer uses deprecated "sendMessage" function.

--- commands/warranty.js
+++ commands/warranty.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 doNotDelete = false;
-message.author.sendMessage(
+message.author.send(
 	"This program is distributed in the hope that it will be useful,\n" +
 	"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
 	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
