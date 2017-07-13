145d11d346a5c84f557fcb298528667463b984b8
Blake <bsward8@gmail.com>
2017-07-13 14:04:15 +1000

    Updated equip to include more useful arguments.
    'list' now shows all of the badges you own.
    'weapon' slot is used for equipping weapons in challenges.

--- commands/equip.js
+++ commands/equip.js
@@ -1,41 +1,56 @@
 const fs = require('fs');
 
 exports.run = (client, message, args) => {
-	
-	message.delete();
-	
-var num = args[0];
-var badge = args[1].toString();
-
-let badgesA = JSON.parse(fs.readFileSync('./badges.json', 'utf8'));
+let badgesA = JSON.parse(fs.readFileSync('./data/badges/Badge Tracker.json', 'utf8'));
 let badgesP = JSON.parse(fs.readFileSync('./data/profile/profile-background.json', 'utf8'));
 var badges = JSON.parse(fs.readFileSync('./slots.json', 'utf8'));
+var badgesC = JSON.parse(fs.readFileSync('./data/challenge/equipment.json', 'utf8'));
 
-		// if the user has no badges, init to false.
-		if (!badges[message.author.id])
-			badges[message.author.id] = {
-				slot1: "empty",
-				slot2: "empty",
-				slot3: "empty",
-				slot4: "empty",
-				slot5: "empty",
-				slot6: "empty"
+if (!badgesC[message.author.id])
+			badgesC[message.author.id] = {
+				weapon: "none"
 			};
-			
-		// if the user has no badges, init to false.
-		if (!badgesP[message.author.id])
-			badgesP[message.author.id] = {
-				background: "default"
-			};
-			
-		var userSlots = badges[message.author.id];
-		var userBadges = badgesA[message.author.id];
-		var userProfile = badgesP[message.author.id];
 
-		if (num == "list") {
-		message.reply(":white_check_mark: **OK:** These are the badges that you currently have: " + badgesA[message.author.id].toString());
-		return;
-		}
+// if the user has no badges, init to false.
+if (!badges[message.author.id])
+	badges[message.author.id] = {
+		slot1: "empty",
+		slot2: "empty",
+		slot3: "empty",
+		slot4: "empty",
+		slot5: "empty",
+		slot6: "empty"
+	};
+
+// if the user has no badges, init to false.
+if (!badgesP[message.author.id])
+	badgesP[message.author.id] = {
+		background: "default"
+	};
+
+var userSlots = badges[message.author.id];
+var userBadges = badgesA[message.author.id];
+var userProfile = badgesP[message.author.id];
+var userChallenge = badgesC[message.author.id];
+
+message.delete();
+
+if (args == "list") {
+let tosend = []
+for (var key in userBadges) {
+    var value = userBadges[key];
+
+    if (value > 0) {
+	tosend.push(key)
+	}
+}
+
+message.reply(":white_check_mark: **OK:** These are the badges that you currently own: **" + tosend + "**.");
+return;
+}
+
+var num = args[0];
+var badge = args[1].toString();
 
 		if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5 || num == 6) {
 		if (eval(`userBadges.${badge} == 1`) == true) {
@@ -46,7 +61,7 @@ var badges = JSON.parse(fs.readFileSync('./slots.json', 'utf8'));
 		return;
 		}
 		}
-							
+
 		if (num == "background") {
 		eval(`userProfile.background = "${badge}"`);
 		fs.writeFile('./data/profile/profile-background.json', JSON.stringify(badgesP, null, 2), function(err) {
@@ -58,8 +73,23 @@ var badges = JSON.parse(fs.readFileSync('./slots.json', 'utf8'));
 		return;
 		//message.reply(":no_entry_sign: **NOPE:** You can't equip this background because you don't own it or it doesn't exist.");
 		}
-		
-			
+
+		if (num == "weapon") {
+		// all possible weapons:
+		// wooden, stone, iron, diamond, master
+		// sub sword: gold, gold sword will stun user with special ability
+		eval(`userChallenge.weapon = "${badge}"`);
+		fs.writeFile('./data/challenge/equipment.json', JSON.stringify(badgesC, null, 2), function(err) {
+				if (err) {
+					console.error(err)
+				}
+			});
+			message.reply(":white_check_mark: **OK:** You've successfully equipped the weapon **" + badge + "**. This can be used in challenges.");
+		return;
+		//message.reply(":no_entry_sign: **NOPE:** You can't equip this background because you don't own it or it doesn't exist.");
+		}
+
+
 		if (num == "all") {
 			if (badge == "empty") {
 			userSlots.slot1 = "empty";
@@ -68,16 +98,19 @@ var badges = JSON.parse(fs.readFileSync('./slots.json', 'utf8'));
 			userSlots.slot4 = "empty";
 			userSlots.slot5 = "empty";
 			userSlots.slot6 = "empty";
-			message.reply(":white_check_mark: **OK:** You've successfully unequipped **all** of your badges.");
+			userProfile.background = "default";
+			message.reply(":white_check_mark: **OK:** You've successfully unequipped **all** of your items.");
+			} else {
+			eval(`userSlots.slot1 = "${badge}"`);
+			eval(`userSlots.slot2 = "${badge}"`);
+			eval(`userSlots.slot3 = "${badge}"`);
+			eval(`userSlots.slot4 = "${badge}"`);
+			eval(`userSlots.slot5 = "${badge}"`);
+			eval(`userSlots.slot6 = "${badge}"`);
+			message.reply(`:white_check_mark: **OK:** You've successfully equipped the badge **${badge}** in to *all* of your slots.`);
 			}
-		} 
-		
-		if (badge == "empty") {
-		message.reply(":white_check_mark: **OK:** You've successfully unequipped any badge in slot **" + num + "**.");
 		}
-		
 
-		
 		fs.writeFile('./slots.json', JSON.stringify(badges, null, 2), function(err) {
 				if (err) {
 					console.error(err)
