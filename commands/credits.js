6a826cc5363a67c4e12e6501c11313348cf1e8e0
Blake <bsward8@gmail.com>
2017-08-30 18:17:21 +1000

    New credits command

--- commands/credits.js
+++ commands/credits.js
@@ -1,6 +1,19 @@
+const Discord = require('discord.js');
+
 exports.run = (client, message, args) => {
 message.delete();
-message.channel.send("hold on");
+const embed = new Discord.MessageEmbed();
+embed.setAuthor("ᴄʀᴇᴅɪᴛs » zBot, created by zBlake#6715", client.user.avatarURL( {format: 'png'} ));
+embed.setColor("#c64ed3");
+var msg = "<:vicr123:351265732434329600> **vicr123** - for providing a codebase for zBot"
+msg += "\n<:aren:351271085540507650> **Aren** - help with console code"
+msg += "\n<:bdestin:351271085687177216> **bdestin** - providing code for music command"
+embed.addField("Special Thanks", msg)
+
+var msg = "Badge icons provided courtesy of https://www.flaticon.com/\nBackground images on profiles taken from Google Images"
+embed.addField("Images", msg)
+
+message.channel.send({ embed })
 }
 
 let command = 'credits'
