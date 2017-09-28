7ff372b462e585bfeee8153e45d61b7a8ed30807
Blake <bsward8@gmail.com>
2017-09-28 22:51:32 +1000

    Another website added in UpdateStats

--- structures/general/UpdateStats.js
+++ structures/general/UpdateStats.js
@@ -8,13 +8,23 @@ class UpdateStats {
 		const snekfetch = require('snekfetch')
 
 	snekfetch.post('https://discordbots.org/api/bots/345766303052857344/stats')
-	  .set('Authorization', api.dbot)
-	  .send({ server_count: client.guilds.size })
-	  .then(log('Updated dbots.org status.', logType.info))
-	  .catch(e => log('Unable to update dbots.org status. ' + e, logType.critical));
 
+	  .set('Authorization', api.dbot())
+	  .send({ server_count: client.guilds.size })
+	  .then(log('Updated discordbots.org status.', logType.info))
+	  .catch(e => log('Unable to update discordbots.org status. ' + e, logType.critical));
 		return;
 		}
+
+	static updateDLB() {
+		const snekfetch = require('snekfetch')
+		snekfetch.post('https://bots.discordlist.net/api')
+		.send({ token: api.dlb() })
+		.send({ servers: client.guilds.size })
+		.then(log('Updated bots.discordlist.net status.', logType.info))
+	  .catch(e => log('Unable to update bots.discordlist.net status. ' + e, logType.critical));
+		return;
+	}
 }
 
 module.exports = UpdateStats;
