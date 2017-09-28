9656fb41e911ea2d6e14638abe1a4fe6a24081d6
Blake <bsward8@gmail.com>
2017-09-28 22:56:58 +1000

    Minor updates to 'find'

--- commands/find.js
+++ commands/find.js
@@ -1,34 +1,31 @@
 const Find = require('./../structures/general/UserFinder.js');
 const Discord = require('discord.js');
+var rancheck = false;
 
 exports.run = (client, message, args) => {
-	message.delete();
 	args = args.toString()
-	users = Find.getUserUsernames(args)
+	message.delete();
+	var users = Find.getUserUsernames(args, message.guild)
+
 	const embed = new Discord.MessageEmbed();
 	embed.setAuthor("ᖴɪɴᴅ » ", client.user.avatarURL( {format: "png"} ));
 	embed.setColor("#8babdd");
 	if (users.length == 1) {
 	var msg = users
-	embed.addField(users.length + " users found containing '" + args + "'", msg)
-	}
-	if (users.length > 1 && users.length < 6) {
+	embed.addField(users.length + " user found containing '" + args + "'", msg)
+} else if (users.length > 1 && users.length < 6) {
 	var msg = users.join('\n')
 	embed.addField(users.length + " users found containing '" + args + "'", msg)
-	}
-	if (users.length > 1 && users.length > 6) {
+} else if (users.length > 1 && users.length > 6) {
 	var msg = users.slice(0,5).join('\n')
 	embed.addField(users.length + " users found containing '" + args + "'", msg)
-	}
-	if (users.length < 1) {
+} else if (users.length < 1) {
 	embed.addField(users.length + " users found containing '" + args + "'", "\u200B")
-	}
-	if (users.length > 10) {
+	embed.setFooter("Check for spelling, and try to generalize keywords to get more results.")
+} else if (users.length > 10) {
 		embed.setFooter("Please narrow your query.")
 	}
-	if (users.length < 1) {
-		embed.setFooter("Check for spelling, and try to generalize keywords to get more results.")
-	}
+
 	message.channel.send({ embed })
 }
 
