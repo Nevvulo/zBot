99d21732eed7dfb98d903bec53a700b518cc2971
Blake <bsward8@gmail.com>
2017-09-28 22:54:20 +1000

    'dog' is fine

--- commands/dog.js
+++ commands/dog.js
@@ -3,7 +3,7 @@ const {get} = require("snekfetch");
 exports.run = (client, message, args) => {
 	message.delete();
     get("https://random.dog/woof.json").then(res => {
-        message.channel.send(":white_check_mark: **OK:** :dog: **|** *Here is your random doggo!*\n" + res.body.url);
+        message.channel.send(":white_check_mark: **OK:** :dog: **|** *Here is your random dog!*\n" + res.body.url);
     });
 };
 
