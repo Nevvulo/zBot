e121016762915326cc1dee8dc240d7708604d5ef
Blake <bsward8@gmail.com>
2017-09-28 22:49:00 +1000

    Help now shows "You can't run this command" on owner only commands

--- structures/general/Help.js
+++ structures/general/Help.js
@@ -20,6 +20,12 @@ class Help {
 				embed.setDescription(":information_source: Information about `" + command + "`.\n:no_entry_sign: You don't have permission to run this command.")
 				embed.setColor("#f48841");
 			}
+
+			if (help.permission == "owner") {
+				embed.setDescription(":information_source: Information about `" + command + "`.\n:no_entry_sign: Only " + guild.owner.displayName + " can run this command.")
+				embed.setColor("#e25a28");
+			}
+
 			embed.addField("**Description**", help.desc);
 			embed.addField("**Usage**", Settings.getValue(guild, "prefix") + help.usage);
 			if (help.example !== undefined) {
