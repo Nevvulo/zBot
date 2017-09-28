aa6cf03016322d108d6be4e4abcec7d9c176d63f
Blake <bsward8@gmail.com>
2017-09-28 22:53:40 +1000

    Update to 'rm' includes new 'bot' parameter that requires more work

--- commands/rm.js
+++ commands/rm.js
@@ -2,18 +2,43 @@ var deleteEverythingConfirm = false;
 
 exports.run = (client, message, args) => {
  doNotDelete = false;
-var num = parseInt(args);
+var num = (args[0] == undefined ? "" : parseInt(args[0]))
+var parameters = (args[1] == undefined ? "" : args[1].toString())
 
 if (num > 99) {
   return message.reply(":no_entry_sign: **NOPE:** Unfortunately, I can't delete more than 99 messages at a time.");
+}
+
+if (parameters == "bot") {
+  var count = 0;
+  deleteMessages();
+  function deleteMessages() {
+  const filter = message => message.author.bot == true;
+  message.channel.messages.fetch({ limit: num }).then(messages => {
+  const filteredMessages = messages.filter(filter);
+  message.channel.send(":white_check_mark: **OK:** I successfully deleted " + eval(messages.size - 1) + " messages created by bots.").then(message => {
+      message.delete({ timeout: 8000 });
+    });
+  message.channel.bulkDelete(filteredMessages);
+  return;
+}).catch(function(reason) {
+  count += 1
+  if (count < 9) {
+  deleteMessages();
+} else {
+  message.channel.send(":no_entry_sign: **ERROR:** There doesn't seem to be any messages from bots in this channel.");
+  return;
+}
+});
+}
 }
 
 if (num != args) {
 	message.reply(":no_entry_sign: **ERROR:** You need to provide the amount of messages you want to delete.");
 } else {
 	num = num + 1; //Also remove the rm command
-	message.channel.bulkDelete(num).then(function() {
-		message.channel.send(":white_check_mark: **OK:** I successfully deleted " + eval(num - 1) + " messages.").then(message => {
+	message.channel.bulkDelete(num).then(function(messages) {
+		message.channel.send(":white_check_mark: **OK:** I successfully deleted " + eval(messages.size - 1) + " messages.").then(message => {
         message.delete({ timeout: 8000 });
       });
 	}).catch(function(reason) {
